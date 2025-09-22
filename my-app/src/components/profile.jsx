372a3a33a0c4781aa69af34df8edd32295d428ac
juan david barba <juandavidbarbadev@gmail.com>
2025-09-22 11:37:19 -0500

    se modifico el enlace de la cv

--- my-app/src/components/profile.jsx
+++ my-app/src/components/profile.jsx
@@ -16,7 +16,7 @@ function Profile({ data }) {
           {data.profession ? data.profession : "Sin profesión registrada"}
         </p>
         <p className="text-gray-700 leading-relaxed">{data.description}</p>
-        <a href="https://drive.google.com/uc?export=download&id=1i95hnRIL70rShBQRTnD037AVpqt2jEQ0"
+        <a href="https://drive.google.com/uc?export=download&id=1Q9kjs6PmRQkDlLtFxXRvlA9XBqZqmywQ"
           target="_blank"
           rel="noopener noreferrer"
           className="inline-block px-6 py-3 text-white font-semibold rounded-lg shadow-md bg-gradient-to-r from-blue-600 to-blue-800 hover:from-blue-700 hover:to-blue-900 hover:shadow-lg transform hover:-translate-y-1 transition-all duration-300"
